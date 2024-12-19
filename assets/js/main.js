8b3e8ef4b56218d3938403218d6394d7528f3ec6
Kyle Mabaso <kylemabaso@gmail.com>
2024-12-19 14:12:36 +0200

    Added additional sections to the layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,121 @@ $(document).ready(function () {
 		ResCarousel(ell, Parent, slide);
 	}
 });
+
+const monthlyBtn = document.getElementById('monthly');
+const yearlyBtn = document.getElementById('yearly');
+const pricingCards = document.getElementById('pricing-cards');
+
+monthlyBtn.addEventListener('click', () => {
+	monthlyBtn.classList.add('active');
+	yearlyBtn.classList.remove('active');
+	pricingCards.innerHTML = `
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Individual</h4>
+                        <h2>R24.75</h2>
+                        <p>Monthly</p>
+                        <hr class="text-white my-3">
+                        <p>1 to 5 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Freelance</h4>
+                        <h2>R26.45</h2>
+                        <p>Monthly</p>
+                        <hr class="text-white my-3">
+                        <p>6 to 10 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Small Businesss</h4>
+                        <h2>R28.75</h2>
+                        <p>Monthly</p>
+                        <hr class="text-white my-3">
+                        <p>11 to 50 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Corporate</h4>
+                        <h2>R345</h2>
+                        <p>Monthly</p>
+                        <hr class="text-white my-3">
+                        <p>101 to 200 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Custom</h4>
+                        <h2>Special Request</h2>
+                        <p>Monthly</p>
+                        <hr class="text-white my-3">
+                        <p>For more than 200 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+            `;
+});
+
+yearlyBtn.addEventListener('click', () => {
+	yearlyBtn.classList.add('active');
+	monthlyBtn.classList.remove('active');
+	pricingCards.innerHTML = `
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Individual</h4>
+                        <h2>R24.75</h2>
+                        <p>Annually</p>
+                        <hr class="text-white my-3">
+                        <p>1 to 5 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Freelance</h4>
+                        <h2>R26.45</h2>
+                        <p>Annually</p>
+                        <hr class="text-white my-3">
+                        <p>6 to 10 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Small Businesss</h4>
+                        <h2>R28.75</h2>
+                        <p>Annually</p>
+                        <hr class="text-white my-3">
+                        <p>11 to 50 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Corporate</h4>
+                        <h2>R345</h2>
+                        <p>Annually</p>
+                        <hr class="text-white my-3">
+                        <p>101 to 200 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-4">
+                    <div class="pricing-card">
+                        <h4>Custom</h4>
+                        <h2>Special Request</h2>
+                        <p>Annually</p>
+                        <hr class="text-white my-3">
+                        <p>For more than 200 employees</p>
+                        <button class="btn-get-started">Get Started</button>
+                    </div>
+                </div>
+            `;
+});
