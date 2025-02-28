68c4f8a2ca5d66bb18d7a757f249e8dd2c26a215
Nqobile-coder <nqobilez96@icloud.com>
2025-02-28 14:04:58 +0200

    Hangouts video
    Replaced the first video on the hangouts with Prof Phakeng's interview.

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
 							<div class="item">
 								<div class="pad15">
 									<iframe width="100%" height="315"
-										src="https://www.youtube.com/embed/ElUTa16Sr0g?si=VB3R5tMInea4sx-N"
+										src="https://www.youtube.com/watch?v=J10c4GtkZy0"
 										title="YouTube video player" frameborder="0"
 										allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 										referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
