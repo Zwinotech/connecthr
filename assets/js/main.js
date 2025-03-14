1ffd35b3c2af05116a4ca5a9b9fef7beceebbef4
nokuphiwa-mk <mkhizenokuphiwa96@gmail.com>
2025-03-14 10:30:46 +0200

    Logo, color, contact form.
    Changed the logo.
    Changed the color of the website from purple into pink.
    Added href links to the  email address, contact as well as location to make it convenient for users.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,122 +1,122 @@
 $(document).ready(function () {
-	var itemsMainDiv = '.multiCarousel';
-	var itemsDiv = '.multiCarousel-inner';
-	var itemWidth = '';
+  var itemsMainDiv = ".multiCarousel";
+  var itemsDiv = ".multiCarousel-inner";
+  var itemWidth = "";
 
-	$('.leftLst, .rightLst').click(function () {
-		var condition = $(this).hasClass('leftLst');
-		if (condition) click(0, this);
-		else click(1, this);
-	});
+  $(".leftLst, .rightLst").click(function () {
+    var condition = $(this).hasClass("leftLst");
+    if (condition) click(0, this);
+    else click(1, this);
+  });
 
-	ResCarouselSize();
+  ResCarouselSize();
 
-	$(window).resize(function () {
-		ResCarouselSize();
-	});
+  $(window).resize(function () {
+    ResCarouselSize();
+  });
 
-	//this function define the size of the items
-	function ResCarouselSize() {
-		var incno = 0;
-		var dataItems = 'data-items';
-		var itemClass = '.item';
-		var id = 0;
-		var btnParentSb = '';
-		var itemsSplit = '';
-		var sampwidth = $(itemsMainDiv).width();
-		var bodyWidth = $('body').width();
-		$(itemsDiv).each(function () {
-			id = id + 1;
-			var itemNumbers = $(this).find(itemClass).length;
-			btnParentSb = $(this).parent().attr(dataItems);
-			itemsSplit = btnParentSb.split(',');
-			$(this)
-				.parent()
-				.attr('id', 'multiCarousel' + id);
+  //this function define the size of the items
+  function ResCarouselSize() {
+    var incno = 0;
+    var dataItems = "data-items";
+    var itemClass = ".item";
+    var id = 0;
+    var btnParentSb = "";
+    var itemsSplit = "";
+    var sampwidth = $(itemsMainDiv).width();
+    var bodyWidth = $("body").width();
+    $(itemsDiv).each(function () {
+      id = id + 1;
+      var itemNumbers = $(this).find(itemClass).length;
+      btnParentSb = $(this).parent().attr(dataItems);
+      itemsSplit = btnParentSb.split(",");
+      $(this)
+        .parent()
+        .attr("id", "multiCarousel" + id);
 
-			if (bodyWidth >= 1200) {
-				incno = itemsSplit[3];
-				itemWidth = sampwidth / incno;
-			} else if (bodyWidth >= 992) {
-				incno = itemsSplit[2];
-				itemWidth = sampwidth / incno;
-			} else if (bodyWidth >= 768) {
-				incno = itemsSplit[1];
-				itemWidth = sampwidth / incno;
-			} else {
-				incno = itemsSplit[0];
-				itemWidth = sampwidth / incno;
-			}
-			$(this).css({
-				transform: 'translateX(0px)',
-				width: itemWidth * itemNumbers,
-			});
-			$(this)
-				.find(itemClass)
-				.each(function () {
-					$(this).outerWidth(itemWidth);
-				});
+      if (bodyWidth >= 1200) {
+        incno = itemsSplit[3];
+        itemWidth = sampwidth / incno;
+      } else if (bodyWidth >= 992) {
+        incno = itemsSplit[2];
+        itemWidth = sampwidth / incno;
+      } else if (bodyWidth >= 768) {
+        incno = itemsSplit[1];
+        itemWidth = sampwidth / incno;
+      } else {
+        incno = itemsSplit[0];
+        itemWidth = sampwidth / incno;
+      }
+      $(this).css({
+        transform: "translateX(0px)",
+        width: itemWidth * itemNumbers,
+      });
+      $(this)
+        .find(itemClass)
+        .each(function () {
+          $(this).outerWidth(itemWidth);
+        });
 
-			$('.leftLst').addClass('over');
-			$('.rightLst').removeClass('over');
-		});
-	}
+      $(".leftLst").addClass("over");
+      $(".rightLst").removeClass("over");
+    });
+  }
 
-	//this function used to move the items
-	function ResCarousel(e, el, s) {
-		var leftBtn = '.leftLst';
-		var rightBtn = '.rightLst';
-		var translateXval = '';
-		var divStyle = $(el + ' ' + itemsDiv).css('transform');
-		var values = divStyle.match(/-?[\d\.]+/g);
-		var xds = Math.abs(values[4]);
-		if (e == 0) {
-			translateXval = parseInt(xds) - parseInt(itemWidth * s);
-			$(el + ' ' + rightBtn).removeClass('over');
+  //this function used to move the items
+  function ResCarousel(e, el, s) {
+    var leftBtn = ".leftLst";
+    var rightBtn = ".rightLst";
+    var translateXval = "";
+    var divStyle = $(el + " " + itemsDiv).css("transform");
+    var values = divStyle.match(/-?[\d\.]+/g);
+    var xds = Math.abs(values[4]);
+    if (e == 0) {
+      translateXval = parseInt(xds) - parseInt(itemWidth * s);
+      $(el + " " + rightBtn).removeClass("over");
 
-			if (translateXval <= itemWidth / 2) {
-				translateXval = 0;
-				$(el + ' ' + leftBtn).addClass('over');
-			}
-		} else if (e == 1) {
-			var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
-			translateXval = parseInt(xds) + parseInt(itemWidth * s);
-			$(el + ' ' + leftBtn).removeClass('over');
+      if (translateXval <= itemWidth / 2) {
+        translateXval = 0;
+        $(el + " " + leftBtn).addClass("over");
+      }
+    } else if (e == 1) {
+      var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+      translateXval = parseInt(xds) + parseInt(itemWidth * s);
+      $(el + " " + leftBtn).removeClass("over");
 
-			if (translateXval >= itemsCondition - itemWidth / 2) {
-				translateXval = itemsCondition;
-				$(el + ' ' + rightBtn).addClass('over');
-			}
-		}
-		$(el + ' ' + itemsDiv).css(
-			'transform',
-			'translateX(' + -translateXval + 'px)'
-		);
-	}
+      if (translateXval >= itemsCondition - itemWidth / 2) {
+        translateXval = itemsCondition;
+        $(el + " " + rightBtn).addClass("over");
+      }
+    }
+    $(el + " " + itemsDiv).css(
+      "transform",
+      "translateX(" + -translateXval + "px)"
+    );
+  }
 
-	//It is used to get some elements from btn
-	function click(ell, ee) {
-		var Parent = '#' + $(ee).parent().attr('id');
-		var slide = $(Parent).attr('data-slide');
-		ResCarousel(ell, Parent, slide);
-	}
+  //It is used to get some elements from btn
+  function click(ell, ee) {
+    var Parent = "#" + $(ee).parent().attr("id");
+    var slide = $(Parent).attr("data-slide");
+    ResCarousel(ell, Parent, slide);
+  }
 });
 
-const monthlyBtn = document.getElementById('monthly');
-const yearlyBtn = document.getElementById('yearly');
-const pricingCards = document.getElementById('pricing-cards');
+const monthlyBtn = document.getElementById("monthly");
+const yearlyBtn = document.getElementById("yearly");
+const pricingCards = document.getElementById("pricing-cards");
 
-monthlyBtn.addEventListener('click', () => {
-	monthlyBtn.classList.add('active');
-	yearlyBtn.classList.remove('active');
-	pricingCards.innerHTML = `
+monthlyBtn.addEventListener("click", () => {
+  monthlyBtn.classList.add("active");
+  yearlyBtn.classList.remove("active");
+  pricingCards.innerHTML = `
 				<div class="col-md-4 mb-4">
 					<div class="pricing-card">
 						<h4>Individual</h4>
 						<h2>R21.85</h2>
-						<p>Per Person - Monthly</p>
+						<p>Per Learner - Monthly</p>
 						<hr class="text-white my-3" />
-						<p>1 to 5 employees</p>
+						<p>1 to 5 learners</p>
 						<p>
 							<a class="btn-get-started" href="#contact">Get Started</a>
 						</p>
@@ -126,9 +126,9 @@ monthlyBtn.addEventListener('click', () => {
 					<div class="pricing-card">
 						<h4>Freelance</h4>
 						<h2>R23.00</h2>
-						<p>Per Person - Monthly</p>
+						<p>Per Learner - Monthly</p>
 						<hr class="text-white my-3" />
-						<p>6 to 10 employees</p>
+						<p>6 to 10 learners</p>
 						<p>
 							<a class="btn-get-started" href="#contact">Get Started</a>
 						</p>
@@ -138,9 +138,9 @@ monthlyBtn.addEventListener('click', () => {
 					<div class="pricing-card">
 						<h4>Small Business</h4>
 						<h2>24.75</h2>
-						<p>Per Person - Monthly</p>
+						<p>Per Learner - Monthly</p>
 						<hr class="text-white my-3" />
-						<p>11 to 50 employees</p>
+						<p>11 to 50 learners</p>
 						<p>
 							<a class="btn-get-started" href="#contact">Get Started</a>
 						</p>
@@ -150,9 +150,9 @@ monthlyBtn.addEventListener('click', () => {
 					<div class="pricing-card">
 						<h4>Medium Business</h4>
 						<h2>R26.45</h2>
-						<p>Per Person - Monthly</p>
+						<p>Per Learner - Monthly</p>
 						<hr class="text-white my-3" />
-						<p>51 to 100 employees</p>
+						<p>51 to 100 learners</p>
 						<p>
 							<a class="btn-get-started" href="#contact">Get Started</a>
 						</p>
@@ -162,9 +162,9 @@ monthlyBtn.addEventListener('click', () => {
 					<div class="pricing-card">
 						<h4>Corporate</h4>
 						<h2>R28.75</h2>
-						<p>Per Person - Monthly</p>
+						<p>Per Learner - Monthly</p>
 						<hr class="text-white my-3" />
-						<p>101 to 200 employees</p>
+						<p>101 to 200 learners</p>
 						<p>
 							<a class="btn-get-started" href="#contact">Get Started</a>
 						</p>
@@ -185,10 +185,10 @@ monthlyBtn.addEventListener('click', () => {
             `;
 });
 
-yearlyBtn.addEventListener('click', () => {
-	yearlyBtn.classList.add('active');
-	monthlyBtn.classList.remove('active');
-	pricingCards.innerHTML = `
+yearlyBtn.addEventListener("click", () => {
+  yearlyBtn.classList.add("active");
+  monthlyBtn.classList.remove("active");
+  pricingCards.innerHTML = `
 				<div class="col-md-4 mb-4">
 					<div class="pricing-card">
 						<h4>Individual</h4>
